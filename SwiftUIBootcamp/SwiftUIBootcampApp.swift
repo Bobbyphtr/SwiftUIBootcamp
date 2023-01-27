bff6fad437f26d76e8f7916a1dd8227309ee15d9
Bobbyphtr <bobbypehtrus2@gmail.com>
2023-01-27 16:15:43 +0700

    Initial Commit

---
+++ SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Bobby Pehtrus on 27/01/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
